3a4468efecf40cdc2ac8898247892ec1c4f97bc0
Jill Poole <jillianntish@gmail.com>
2020-01-21 12:03:40 -0600

    implement express-graphQL, no routes

--- aloud/server/index.js
+++ aloud/server/index.js
@@ -1,1 +1,72 @@
-// express server
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const app = express();
+const port = 3000;
+
+//dev visualizer
+const { middleware, visualizer } = require('express-routes-visualizer');
+app.use(
+  '/routes',
+  middleware({ httpMethods: true }),
+  visualizer({ theme: 'burn' })
+)
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+
+type Query {
+  hello: String
+  recordings: [Recording!]!
+  recording(id: ID!): Recording
+  description: String!
+}
+
+type Mutation {
+  createDraft(
+    title: String!,
+    content: String
+    ): Recording
+
+  updateRecording(
+    id: ID!
+    title: String!
+    content: String!
+    published: Boolean!
+  ) : Recording
+
+  deleteRecording(
+    id: ID!
+    ): Recording
+
+  publishRecording(
+    id: ID!
+    ): Recording
+}
+
+type Recording {
+  id: ID!
+  title: String!
+  content: String!
+  published: Boolean!
+}
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.get('/', (req, res) => res.send('hello world'));
+
+app.listen(port, () => console.log(`Express app is listening on port ${port}!🛸`))
+console.log('Running a GraphQL API server at http://localhost:3000/graphql');
