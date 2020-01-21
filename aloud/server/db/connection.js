9244d80f958b4f63564b5bc118ac298062253921
addupe <autumndupin@gmail.com>
2020-01-21 15:38:27 -0600

    setup first postgres pool connection

--- aloud/server/db/connection.js
+++ aloud/server/db/connection.js
@@ -1,1 +1,16 @@
-// db connection
+const { Pool } = require('pg')
+
+const db_user = 'postgres';
+const db_name = 'aloud';
+
+const config = {
+  user: db_user,
+  database: db_name,
+  max: 10,
+  idleTimeoutMillis: 30000
+}
+
+const pool = new Pool(config);
+
+module.exports.pool = pool;
+module.exports.db_name = db_name;
